5528427ba36eb2c84074dfabe126a082852f9d87
ax21078 <shenjiayingadm@outlook.com>
2025-04-19 00:06:34 +0100

    update readme

--- Assessment/Report/README.md
+++ Assessment/Report/README.md
@@ -1,8 +1,15 @@
-# Report title goes here
-
-Name of author, link to github repo with project work in / link to Edge Impulse projects
+# Uk coin classification
+Jiaying Shen
+Github link: 
+Edge impulse link:
 
 ## Introduction
+This project aims to develop Egde Impulse and transfer learning based UK coin classification system, which ables to classify 5 catrgories coin (1 penty, 5 pence, 10 pence, 20 pence, 50 pence) with over 80% accuracy. Initial idea comes from the scenario that UK coin always choas in daily life, it's hard to quickly tell them apart especially when checking out. 
+
+Traditional vending machines identify coins based on their size (Segrave, 2015). However, many coins have similar sizes but different values, which leads to recognition errors. This problem becomes worse when coins are worn out or stacked together. By contrast, manual sorting is a way to solve but time-consuming. In that case, an efficient classification system needed. 
+
+Previous studies have shown that convolutional neural networks (CNNs) can perform better than the human eye in recognizing coins (Xiang and Yan, 2021). Building on this, Jahan et al. (2023) proposed a deep learning model using MobileNetV2 for global coin detection, which is a compact and efficient neural network based on depth-wise separable convolutions, achieving an accuracy of 84.24%.
+
 - an overview of what the project does
 - your inspiration for making the project 
 - examples that it is based on. 
@@ -10,9 +17,7 @@ Name of author, link to github repo with project work in / link to Edge Impulse 
 *Tip: probably ~200 words and images are good!*
 
 ## Research Question
-What is the problem you are trying to solve
-
-*Tip: probably 1 or 2 sentences*
+Is it able to develop a system that run on mobile phone to quick classify the UK coin?
 
 ## Application Overview
 Thinking back to the various application diagrams you have seen through the module - how would you describe an overview of the building blocks of your project - how do they connect, what do the component parts include.
@@ -20,6 +25,8 @@ Thinking back to the various application diagrams you have seen through the modu
 *Tip: probably ~200 words and a diagram is usually good to convey your design!*
 
 ## Data
+The data comes from online open source and self-collection.
+
 Describe what data sources you have used and any cleaning, wrangling or organising you have done. Including some examples of the data helps others understand what you have been working with.
 
 *Tip: probably ~200 words and images of what the data 'looks like' are good!*
@@ -42,9 +49,11 @@ Synthesis the main results and observations you made from building the project. 
 ## Bibliography
 *If you added any references then add them in here using this format:*
 
-1. Last name, First initial. (Year published). Title. Edition. (Only include the edition if it is not the first edition) City published: Publisher, Page(s). http://google.com
+1. Jahan, Z., Nazia Parween, Agrawal, A.P., Choudhary, A., Raj, G. and Aziz Deraman (2023). Deep Learning Based World Coin Currency Detection. Algorithms for intelligent systems, pp.439–449. doi:https://doi.org/10.1007/978-981-99-1620-7_35.
 
-2. Last name, First initial. (Year published). Title. Edition. (Only include the edition if it is not the first edition) City published: Publisher, Page(s). http://google.com
+2. Xiang, Y., Yan, W.Q. Fast‐moving coin recognition using deep learning. Multimed Tools Appl 80, 24111–24120 (2021). https://doi.org/10.1007/s11042-021-10857-5
+
+3. Segrave, K. (2015) Vending machines: an American social history. Jefferson, NC: McFarland.
 
 *Tip: we use [https://www.citethisforme.com](https://www.citethisforme.com) to make this task even easier.* 
 
--- Code/train.py
+++ Code/train.py
@@ -1,25 +1,24 @@
-import math, requests, os, random
+
+import math, requests
 from pathlib import Path
 import tensorflow as tf
 from tensorflow.keras import Model
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import (
     Dense, InputLayer, Dropout, Conv1D, Flatten, Reshape, MaxPooling1D, BatchNormalization,
-    Conv2D, GlobalMaxPooling2D, Lambda, GlobalAveragePooling2D
-)
-from tensorflow.keras.optimizers.legacy import Adam
+    Conv2D, GlobalMaxPooling2D, Lambda, GlobalAveragePooling2D)
+from tensorflow.keras.optimizers.legacy import Adam, Adadelta
+from tensorflow.keras.losses import categorical_crossentropy
 from tensorflow.keras.callbacks import EarlyStopping, TensorBoard, ModelCheckpoint
-import matplotlib.pyplot as plt
-
 # set TensorBoard log
 log_dir = os.path.join("logs", "fit")
 tensorboard_callback = TensorBoard(log_dir=log_dir, histogram_freq=1)
+
 
 sys.path.append('./resources/libraries')
 import ei_tensorflow.training
 
 WEIGHTS_PATH = './transfer-learning-weights/edgeimpulse/MobileNetV2.0_35.96x96.grayscale.bsize_64.lr_0_005.epoch_260.val_loss_3.10.val_accuracy_0.35.hdf5'
-BEST_MODEL_PATH = './best_model.hdf5'  
 
 # Download the model weights
 root_url = 'https://cdn.edgeimpulse.com/'
@@ -36,32 +35,42 @@ if not p.exists():
 
 INPUT_SHAPE = (96, 96, 1)
 
+
 base_model = tf.keras.applications.MobileNetV2(
-    input_shape=INPUT_SHAPE, alpha=0.35,
-    weights=WEIGHTS_PATH
+    input_shape = INPUT_SHAPE, alpha=0.35,
+    weights = WEIGHTS_PATH
 )
 
 base_model.trainable = False
 
 model = Sequential()
 model.add(InputLayer(input_shape=INPUT_SHAPE, name='x_input'))
+# Don't include the base model's top layers
 last_layer_index = -3
 model.add(Model(inputs=base_model.inputs, outputs=base_model.layers[last_layer_index].output))
 model.add(Reshape((-1, model.layers[-1].output.shape[3])))
-model.add(Dense(16, activation='relu'))
-model.add(Dropout(0.1))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.3))
 model.add(Flatten())
 model.add(Dense(classes, activation='softmax'))
 
+
 # Implements the data augmentation policy
 def augment_image(image, label):
+    # Flips the image randomly
     image = tf.image.random_flip_left_right(image)
+
+    # Increase the image size, then randomly crop it down to
+    # the original dimensions
     resize_factor = random.uniform(1, 1.2)
     new_height = math.floor(resize_factor * INPUT_SHAPE[0])
     new_width = math.floor(resize_factor * INPUT_SHAPE[1])
     image = tf.image.resize_with_crop_or_pad(image, new_height, new_width)
     image = tf.image.random_crop(image, size=INPUT_SHAPE)
+
+    # Vary the brightness of the image
     image = tf.image.random_brightness(image, max_delta=0.2)
+
     return image, label
 
 train_dataset = train_dataset.map(augment_image, num_parallel_calls=tf.data.AUTOTUNE)
@@ -69,18 +78,19 @@ train_dataset = train_dataset.map(augment_image, num_parallel_calls=tf.data.AUTO
 BATCH_SIZE = args.batch_size or 32
 EPOCHS = args.epochs or 60
 LEARNING_RATE = args.learning_rate or 0.0005
+# If True, non-deterministic functions (e.g. shuffling batches) are not used.
+# This is False by default.
 ENSURE_DETERMINISM = args.ensure_determinism
 if not ENSURE_DETERMINISM:
     train_dataset = train_dataset.shuffle(buffer_size=BATCH_SIZE*4)
 prefetch_policy = 1 if ENSURE_DETERMINISM else tf.data.AUTOTUNE
-train_dataset = train_dataset.batch(BATCH_SIZE).prefetch(prefetch_policy)
-validation_dataset = validation_dataset.batch(BATCH_SIZE).prefetch(prefetch_policy)
-
+train_dataset = train_dataset.batch(BATCH_SIZE, drop_remainder=False).prefetch(prefetch_policy)
+validation_dataset = validation_dataset.batch(BATCH_SIZE, drop_remainder=False).prefetch(prefetch_policy)
 callbacks = [
     EarlyStopping(
         monitor='val_accuracy',    
         min_delta=0.005,           
-        patience=10,               
+        patience=8,               
         verbose=1,                 
         restore_best_weights=True  
     ),
@@ -88,22 +98,23 @@ callbacks = [
     ModelCheckpoint(BEST_MODEL_PATH, monitor='val_accuracy', save_best_only=True, verbose=1)  # keep the best model
 ]
 
-model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=LEARNING_RATE),
-              loss='categorical_crossentropy',
-              metrics=['accuracy'])
 
-# initial training
-history = model.fit(train_dataset, validation_data=validation_dataset, epochs=EPOCHS, verbose=2, callbacks=callbacks)
-print('\nInitial training done.', flush=True)
+model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=LEARNING_RATE),
+                loss='categorical_crossentropy',
+                metrics=['accuracy'])
+model.fit(train_dataset, validation_data=validation_dataset, epochs=EPOCHS, verbose=2, callbacks=callbacks)
+
+print('')
+print('Initial training done.', flush=True)
 
 # How many epochs we will fine tune the model
 FINE_TUNE_EPOCHS = 10
 # What percentage of the base model's layers we will fine tune
 FINE_TUNE_PERCENTAGE = 65
 
-print(f'Fine-tuning best model for {FINE_TUNE_EPOCHS} epochs...', flush=True)
+print('Fine-tuning best model for {} epochs...'.format(FINE_TUNE_EPOCHS), flush=True)
 
-#  Load best model from initial training
+# Load best model from initial training
 model = ei_tensorflow.training.load_best_model(BEST_MODEL_PATH)
 
 # Determine which layer to begin fine tuning at
@@ -112,17 +123,20 @@ fine_tune_from = math.ceil(model_layer_count * ((100 - FINE_TUNE_PERCENTAGE) / 1
 
 # Allow the entire base model to be trained
 model.trainable = True
+# Freeze all the layers before the 'fine_tune_from' layer
 for layer in model.layers[:fine_tune_from]:
     layer.trainable = False
 
 model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.000045),
-              loss='categorical_crossentropy',
-              metrics=['accuracy'])
+                loss='categorical_crossentropy',
+                metrics=['accuracy'])
 
-history = model.fit(
-    train_dataset, 
-    validation_data=validation_dataset, 
-    epochs=FINE_TUNE_EPOCHS, 
-    verbose=2, 
-    callbacks=callbacks
-)
+model.fit(train_dataset,
+                epochs=FINE_TUNE_EPOCHS,
+                verbose=2,
+                validation_data=validation_dataset,
+                callbacks=callbacks,
+                class_weight=None
+            )
+
+ 
